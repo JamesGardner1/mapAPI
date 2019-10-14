95249428a265366913b8800145f8077553ccd4ce
JamesGardner1 <jamesacgardner@yahoo.com>
2019-10-13 23:57:57 -0500

    Got Environment Variable to work!

---
+++ forecast.py
@@ -0,0 +1,19 @@
+import os
+import requests
+from datetime import datetime
+
+
+key = os.environ.get('WEATHER_KEY')
+query = {'q': 'minneapolis,us', 'units': 'imperial', 'appid': key}
+
+url = 'http://api.openweathermap.org/data/2.5/forecast'
+data = requests.get(url, params=query).json()
+
+forecast_items = data['list']
+
+for forecast in forecast_items:
+    timestamp = forecast['dt']
+    date = datetime.fromtimestamp(timestamp)
+    temp = forecast['main']['temp']
+    print(f'at {date} temp is {temp}')
+
--- weather.py
+++ weather.py
@@ -2,11 +2,14 @@ import requests
 import os
 
 
-def main():
+key = os.environ.get('WEATHER_KEY')
+city = input('What place would you like to know the weather in?')
+query = {'q': city, 'units': 'imperial', 'appid': key}
 
-    key = os.environ.get('WEATHER_KEY')
-    print(key)
- 
-main()
+url = 'http://api.openweathermap.org/data/2.5/weather'
+data = requests.get(url, params=query).json()
+weather_description = data['weather'][0]['description']
 
+temp= data['main']['temp']
+print(f'The weather is {weather_description}, the temperature is {temp:.2f}F.')
 
