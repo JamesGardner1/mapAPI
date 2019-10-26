59e74d7dec7f8d80b42452a37cb122062a93f10a
JamesGardner1 <jamesacgardner@yahoo.com>
2019-10-26 18:24:37 -0500

    finished lab

--- forecast.py
+++ forecast.py
@@ -3,20 +3,20 @@ import requests
 from datetime import datetime
 
 
-def main():
-    key = os.environ.get('WEATHER_KEY')
-    query = {'q': 'minneapolis,us', 'units': 'imperial', 'appid': key}
 
-    url = 'http://api.openweathermap.org/data/2.5/forecast'
-    data = requests.get(url, params=query).json()
+key = os.environ.get('WEATHER_KEY')
+query = {'q': 'minneapolis,us', 'units': 'imperial', 'appid': key}
 
-    forecast_items = data['list']
+url = 'http://api.openweathermap.org/data/2.5/forecast'
+data = requests.get(url, params=query).json()
 
-    for forecast in forecast_items:
-        timestamp = forecast['dt']
-        date = datetime.fromtimestamp(timestamp)
-        temp = forecast['main']['temp']
-        print(f'at {date} temp is {temp}')
+forecast_items = data['list']
+
+for forecast in forecast_items:
+    timestamp = forecast['dt']
+    date = datetime.fromtimestamp(timestamp)
+    temp = forecast['main']['temp']
+    print(f'at {date} temp is {temp}')
 
 
 
--- forecastlab6.py
+++ forecastlab6.py
@@ -5,42 +5,29 @@ import requests
 from datetime import datetime
 
 
-def main():
-    location = get_location()
-    data = get_forecast(location)
-    get_metrics(data)
 
 
-def get_location():
-    ''' Retrieve the location '''
-    location = input('Please provide a city and country code you would like to know the forecast in: ')
-    if ' ' in location:
-        print('Please enter the city and country in this format : \'minneapolis,us\'')
-    else:
-        return location
 
 
-def get_forecast(location):
-    key = os.environ.get('WEATHER_KEY')
-    query = {'q': location, 'units': 'imperial', 'appid': key}
+''' Retrieve the location '''
+location = input('Please provide a city and country code you would like to know the forecast in: ')
 
-    url = 'http://api.openweathermap.org/data/2.5/forecast'
-    data = requests.get(url, params=query).json()
-    return data
+key = os.environ.get('WEATHER_KEY')
+query = {'q': location, 'units': 'imperial', 'appid': key}
 
-def get_metrics(data):
-    key = os.environ.get('WEATHER_KEY')
-    forecast_items = data['list']
-    for forecast in forecast_items:
-        timestamp = forecast['dt']
-        date = datetime.fromtimestamp(timestamp)
-        temp = forecast['main']['temp']
-        weather_description = data['weather'][0]['description']
-        wind = data['wind']['speed']
-        print(f'In {location} it is {temp}')
+url = 'http://api.openweathermap.org/data/2.5/forecast'
+data = requests.get(url, params=query).json()
+forecast_items = data['list']
+weather = forecast_items[0]
+
+for forecast in forecast_items:
+    timestamp = forecast['dt']
+    date = datetime.fromtimestamp(timestamp)
+    temp = forecast['main']['temp']
+    weather_description = weather['weather'][0]['description']
+    wind = weather['wind']['speed']
+    print(f'In {location} at {date} it is {weather_description}, the temperature is {temp}, and the wind speed is {wind}')
 
         
 
 
-if __name__ == '__main__':
-    main()
